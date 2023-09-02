9dbdfc537da700bed50049881e5b3d5049977091
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-02 19:57:57 +0330

    add Comparisons classes

--- js/core.py
+++ js/core.py
@@ -88,5 +88,108 @@ class Not(BoolOp):
     def __init__(self, obj1=None) -> None:
         super().__init__(obj1, op = Constatant.Not)
 
+
+class Comparisons(BuiltIn):
+    def __init__(self, obj1=None, obj2=None, op=None) -> None:
+        self.obj1 = obj1
+        self.obj2 = obj2
+        self.op = op
+
+    @property
+    def obj1(self):
+        return self.__obj1
+
+    @obj1.setter
+    def obj1(self, obj):
+        self.__obj1 = obj
+
+    @property
+    def op(self):
+        return self.__op
+
+    @op.setter
+    def op(self, val: str):
+        self.__op = val
+
+    @property
+    def obj2(self):
+        return self.__obj2
+
+    @obj2.setter
+    def obj2(self, obj):
+        self.__obj2 = obj
+
+    def renderClass(self):
+        return f"({str(self.obj1)} {(self.op)} {(self.obj2)})"
+
+
+class LT(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '<')
+
+
+class LTE(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '<=')
+
+
+class GT(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '>')
+
+
+class GTE(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '>=')
+
+
+class Eq(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '==')
+
+
+class NotEq(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '!=')
+
+
+class Is(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '===')
+
+
+class IsNot(Comparisons):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2, '!==')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     print(Not(And(4,2)))
