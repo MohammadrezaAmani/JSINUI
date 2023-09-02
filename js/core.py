9db694d1d0cc4ad27f3fe40859f0920b6783ad23
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-02 21:25:15 +0330

    add Math Operation

--- js/core.py
+++ js/core.py
@@ -9,6 +9,23 @@ class Constatant:
     And = "&&"
     Or = "||"
     Not = "!"
+    Add = '+'
+    Sub = '-'
+    Mult = '*'
+    Div = '/'
+    FloorDiv = 'Math.floor'
+    Pow = 'Math.pow'
+    Mod = '%'
+    BitAnd = '&'
+    BitOr = '|'
+    BitXor = '^'
+    LShift = '<<'
+    RShift = '>>'
+    Negative = '-'
+    Posetive = '+'
+    BitInvert = '~'
+    
+    
 
 
 class BuiltIn(object):
@@ -70,8 +87,8 @@ class BoolOp(BuiltIn):
         self.__obj2 = convertType(obj)
 
     def renderClass(self):
-        if not self.obj2:
-            return f"{self.op}({self.obj1})"
+        if self.obj2 == None:
+            return f"({self.op}{self.obj1})"
         return f"({str(self.obj1)} {(self.op)} {(self.obj2)})"
 
 
@@ -216,6 +233,145 @@ class Complex(Type):
     def __init__(self, obj=None) -> None:
         super().__init__(obj)
 
+
+class Op(BuiltIn):
+    def __init__(self, obj1=None, obj2=None, op=None, method=None, aug=None) -> None:
+        self.obj1 = obj1
+        self.obj2 = obj2
+        self.method = method
+        self.aug = aug
+        self.op = op
+    
+    @property 
+    def obj1(self):
+        return self.__obj1
+    @obj1.setter
+    def obj1(self, obj):
+        self.__obj1 = convertType(obj)
+        
+    @property 
+    def op(self):
+        return self.__op
+    @op.setter
+    def op(self, val:str):
+        self.__op = val
+        
+    @property 
+    def obj2(self):
+        return self.__obj2
+    @obj2.setter
+    def obj2(self, obj):
+        self.__obj2 = convertType(obj)
+        
+    @property 
+    def method(self):
+        return self.__method
+    @method.setter
+    def method(self, val:str):
+        self.__method = val
+        
+    @property 
+    def aug(self):
+        return self.__aug
+    @aug.setter
+    def aug(self, val:bool):
+        self.__aug = val
+        
+    def renderClass(self):
+        render_text = ''
+        if self.obj2 == None:
+            render_text = f'({self.op}{self.obj1})'
+        elif self.aug:
+            if self.method:
+                render_text = f'{self.obj1} = {self.op}({self.obj1},{self.obj2})'
+            else:
+                render_text = f'{self.obj1} {self.op}= {self.obj2}'
+        else:
+            if self.method:
+                render_text = f'{self.op}({self.obj1},{self.obj2})'
+            else:
+                render_text = f'({self.obj1} {self.op} {self.obj2})'
+
+        return render_text
+
+class Add(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Add,)
+
+
+class Sub(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Sub,)
+
+
+class Mult(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Mult,)
+
+
+class Div(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Div,)
+
+
+class FloorDiv(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.FloorDiv,)
+
+
+class Mod(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Mod,)
+
+
+class Pow(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Pow,)
+
+
+class Negative(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Negative,)
+
+
+class Posetive(Op):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.Posetive,)
+
+
+class BitwiseOp(Op):
+    def __init__(self, obj1=None, obj2=None, op=None, method=None, aug=None) -> None:
+        super().__init__(obj1, obj2, op, method, aug)
+
+
+class LShift(BitwiseOp):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.LShift,)
+
+
+class RShift(BitwiseOp):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.RShift,)
+
+
+class BitAnd(BitwiseOp):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.BitAnd,)
+
+
+class BitOr(BitwiseOp):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.BitOr,)
+
+
+class BitXor(BitwiseOp):
+    def __init__(self, obj1=None, obj2=None) -> None:
+        super().__init__(obj1, obj2,Constatant.BitXor,)
+
+
+class BitInvert(BitwiseOp):
+    def __init__(self, obj1=None) -> None:
+        super().__init__(obj1, obj2=None ,op=Constatant.BitInvert,)
 
 if __name__ == "__main__":
     print(Not(And(4, 2)))
